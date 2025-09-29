288b463fc49f14c07b48b2ee1bc9469e469cdaad
anubhavgirdhar1 <anubhavxp2@gmail.com>
2025-09-29 11:21:38 +0530

    docs: added example for openai

---
+++ examples/openai.py
@@ -0,0 +1,10 @@
+from wrapper.core import Wrapper
+
+openai_client = Wrapper("openai")
+
+response = openai_client.generate(
+    model="gpt-4",
+    prompt="say yes if you are active",
+    stream=True)
+
+print(response)
