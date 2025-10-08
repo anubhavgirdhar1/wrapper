e163d39cb910d72404a54a79326fd4b453908eb5
anubhavgirdhar1 <anubhavxp2@gmail.com>
2025-10-08 19:42:52 +0530

    fix: changed model name in example files

--- examples/bedrock_ex.py
+++ examples/bedrock_ex.py
@@ -3,6 +3,6 @@ from wrapper.core import Wrapper
 bedrock_client = Wrapper('bedrock')
 
 response = bedrock_client.generate(
-    model="claude-sonnet-4-0",
+    model="us.anthropic.claude-3-5-sonnet-20240620-v1:0",
     prompt="Hello, how are you?"
 )
--- examples/openai.py
+++ examples/openai.py
@@ -3,6 +3,6 @@ from wrapper.core import Wrapper
 openai_client = Wrapper("openai")
 
 response = openai_client.generate(
-    model="gpt-4",
+    model="gpt-5",
     prompt="say yes if you are active",
     stream=True)
